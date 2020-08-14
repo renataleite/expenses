3b2b909b9bccbd47fa2d3a6fccd85e6f3c6d5a47
Renata Leite S. Guedes <renataleitesg@gmail.com>
2020-08-14 21:43:28 +0100

    implement initial version

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(
+        child: Text('Versão Inicial'),
+      ),
+    );
   }
 }
