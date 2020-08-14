7c7c0a814f3b849773b875be35cac46eb5e234de
Renata Leite S. Guedes <renataleitesg@gmail.com>
2020-08-14 21:25:30 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
