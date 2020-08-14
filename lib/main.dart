f48fa059a7b5058bc1351583e408f337802865c9
Renata Leite S. Guedes <renataleitesg@gmail.com>
2020-08-14 23:29:28 +0100

    customize list

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,33 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: _transactions
                   .map((tr) => Card(
-                        child: Text(tr.title),
+                        child: Row(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.symmetric(
+                                    horizontal: 15, vertical: 10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                  color: Colors.purple[50],
+                                  width: 2,
+                                )),
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  tr.value.toString(),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: Colors.purple,
+                                  ),
+                                )),
+                            Column(
+                              children: [
+                                Text(tr.title),
+                                Text(tr.date.toString()),
+                              ],
+                            )
+                          ],
+                        ),
                       ))
                   .toList(),
             )
